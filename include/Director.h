e6eae39fef46a2a562c274837f12590e01ab9906
abdullah134 <abdullahaqeel184@gmail.com>
2024-11-21 21:44:24 +0500

    Implement leave application classes and approval logic for Directors and Supervisors

--- include/Director.h
+++ include/Director.h
@@ -1,9 +1,13 @@
 #include "Employee.h"
+#include "DataManager.h"
+
 
 class Director : public Employee
 {
     Director(std::string name, unsigned int id, std::string password, std::string position, std::shared_ptr<AttendanceRecord> attendanceRecord, std::shared_ptr<LeaveApplication> leaveBalance);
     Director(json j);
     bool ApproveApplication(LeaveApplication& leave);
+    bool RejectApplication(LeaveApplication& leave);
+    // vector<LeaveApplication> viewLeaveApplications(std::shared_ptr<DataManager> dataManager);
 
 };
